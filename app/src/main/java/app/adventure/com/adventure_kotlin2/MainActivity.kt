366eb4d5b679d3efe524ffa12670a66b6b37a5cf
Barley <barley@honbabin.com>
2018-07-10 19:14:18 +0900

    Recyclerview adapter

--- app/src/main/java/app/adventure/com/adventure_kotlin2/MainActivity.kt
+++ app/src/main/java/app/adventure/com/adventure_kotlin2/MainActivity.kt
@@ -2,11 +2,44 @@ package app.adventure.com.adventure_kotlin2
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.adapter = MainRecyclerViewAdapter()
+        recyclerView.layoutManager = GridLayoutManager(this, 3)
+    }
+
+    class MainRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(parent!!.context).inflate(R.layout.item_layout, parent, false)
+            return CustomViewHolder(view)
+        }
+
+        class CustomViewHolder(view: View?) : RecyclerView.ViewHolder(view) {
+            var imageView : ImageView? = null
+            init {
+                imageView = view!!.findViewById(R.id.img_item)
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return 4
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            //To change body of created functions use File | Settings | File Templates.
+        }
+
     }
 }
