26a6878fc221f822e597f81a6e1b65fe9151a50f
Barley <barley@honbabin.com>
2018-07-10 15:49:55 +0900

    ConstraintLayout

--- app/src/main/java/app/adventure/com/adventure_kotlin2/MainActivity.kt
+++ app/src/main/java/app/adventure/com/adventure_kotlin2/MainActivity.kt
@@ -1,7 +1,7 @@
 package app.adventure.com.adventure_kotlin2
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
