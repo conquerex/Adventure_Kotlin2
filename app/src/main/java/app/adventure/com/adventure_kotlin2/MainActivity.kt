e695c89a5f99e25af94c522ca5bf4092817673f4
Barley <barley@honbabin.com>
2018-07-10 17:41:30 +0900

    Button and other views

--- app/src/main/java/app/adventure/com/adventure_kotlin2/MainActivity.kt
+++ app/src/main/java/app/adventure/com/adventure_kotlin2/MainActivity.kt
@@ -2,11 +2,16 @@ package app.adventure.com.adventure_kotlin2
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_main.setOnClickListener {
+            var str = edt_main.text
+            txt_main.text = str
+        }
     }
 }
