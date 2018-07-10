9aaa8ed21fe4a4bf2f23eca5ca7383134dc3ed12
Barley <barley@honbabin.com>
2018-07-10 18:22:08 +0900

    Reset for Recyclerview sample

--- app/src/main/java/app/adventure/com/adventure_kotlin2/MainActivity.kt
+++ app/src/main/java/app/adventure/com/adventure_kotlin2/MainActivity.kt
@@ -2,17 +2,11 @@ package app.adventure.com.adventure_kotlin2
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn_main.setOnClickListener {
-            var str = edt_main.text
-            txt_main.text = str
-            img_main.setImageResource(R.drawable.beer_cup)
-        }
     }
 }
