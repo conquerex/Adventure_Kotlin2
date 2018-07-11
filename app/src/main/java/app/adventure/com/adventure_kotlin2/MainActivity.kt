5c47265bb082fdc5e71aeb8dde6dc34bdb8d1c60
Barley <barley@honbabin.com>
2018-07-11 17:58:47 +0900

    ViewPager

--- app/src/main/java/app/adventure/com/adventure_kotlin2/MainActivity.kt
+++ app/src/main/java/app/adventure/com/adventure_kotlin2/MainActivity.kt
@@ -1,14 +1,10 @@
 package app.adventure.com.adventure_kotlin2
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,41 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView.adapter = MainRecyclerViewAdapter()
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        viewPager2.adapter = ViewPagerAdapter(supportFragmentManager)
     }
 
-    class MainRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-        var images = arrayOf(R.drawable.drink_1, R.drawable.drink_2, R.drawable.drink_3,
-                             R.drawable.drink_1, R.drawable.drink_2)
-        var titles = arrayOf("Test11", "Test2", "Test3333", "Test 44", "Test555")
-        var contents = arrayOf("하나...", "둘...", "세엣...", "네에엣.....", "다아서어엇..")
-        override fun getItemCount(): Int {
-            return images.size
+    class ViewPagerAdapter(fm: FragmentManager): FragmentStatePagerAdapter(fm) {
+
+        var fragments = arrayOf(Fragment1(), Fragment2())
+
+        // 화면을 넣어주는 부분
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
         }
 
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            var view = holder as CustomViewHolder
-            view.imageView!!.setImageResource(images[position])
-            view.textView!!.text = titles[position]
-            view.textView2!!.text = contents[position]
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            var view = LayoutInflater.from(parent!!.context).inflate(R.layout.item_layout, parent, false)
-            return CustomViewHolder(view)
-        }
-
-        class CustomViewHolder(view : View?) : RecyclerView.ViewHolder(view) {
-            var imageView : ImageView? = null
-            var textView : TextView? = null
-            var textView2 : TextView? = null
-
-            init {
-                imageView = view!!.findViewById(R.id.img_profile)
-                textView  = view.findViewById(R.id.txt_title)
-                textView2 = view.findViewById(R.id.txt_content)
-            }
+        // 페이지 갯수를 세는 부분
+        override fun getCount(): Int {
+            return fragments.size
         }
 
     }
