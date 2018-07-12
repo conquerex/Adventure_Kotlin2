232fd029d8430f237ff4e628ef49a5f59864ef63
Barley <barley@honbabin.com>
2018-07-12 13:59:31 +0900

    WebView

--- app/src/main/java/app/adventure/com/adventure_kotlin2/MainActivity.kt
+++ app/src/main/java/app/adventure/com/adventure_kotlin2/MainActivity.kt
@@ -2,12 +2,17 @@ package app.adventure.com.adventure_kotlin2
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.naver.com")
 
     }
 
