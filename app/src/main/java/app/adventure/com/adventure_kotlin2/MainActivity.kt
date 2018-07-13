49a4650a36fa0085e9a04ff72a5dec545d2ff1f4
Barley <barley@honbabin.com>
2018-07-13 10:39:33 +0900

    Intent

--- app/src/main/java/app/adventure/com/adventure_kotlin2/MainActivity.kt
+++ app/src/main/java/app/adventure/com/adventure_kotlin2/MainActivity.kt
@@ -1,13 +1,19 @@
 package app.adventure.com.adventure_kotlin2
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_move.setOnClickListener {
+            var intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onStart() {
---
+++ app/src/main/java/app/adventure/com/adventure_kotlin2/SecondActivity.kt
@@ -0,0 +1,13 @@
+package app.adventure.com.adventure_kotlin2
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+}
